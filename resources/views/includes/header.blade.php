2327773f795f4ee9dccff844647d34b04f4de62a
Dasun Dissanayake <dpdasun@gmail.com>
2018-04-09 02:10:21 +0530

    Add responsive sidebar to navigation bar

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -59,8 +59,70 @@
             <!-- Collect the nav links, forms, and other content for toggling -->
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav navbar-right">
-                    <li><a href="#">News</a></li>
-                    <li><a href="#">Promotions</a></li>
+                    @if(Auth::user())
+                        @php
+                            $unread_message_count = \App\ReceiveMessage::where('receiver_id',Auth::user()->id)->where('is_read',0)->count();
+                        @endphp
+                        @if(Auth::user()->username=='admin')
+                            <li role="presentation"
+                                {{Request::segment(2)=='home'?$active='active':$active=''}} class="{{$active}}">
+                                <a href="{{url('/admin/home')}}"><i class="fa fas fa-home" aria-hidden="true"></i>&nbsp;&nbsp;&nbsp;Dashboard</a>
+                            </li>
+                            <li role="presentation"
+                                {{Request::segment(2)=='users'?$active='active':$active=''}} class="{{$active}}">
+                                <a href="{{url('/admin/users')}}"><i class="fa fas fa-users" aria-hidden="true"></i>&nbsp;&nbsp;&nbsp;Users</a>
+                            </li>
+                            <li role="presentation"
+                                {{Request::segment(2)=='applications'?$active='active':$active=''}} class="{{$active}}">
+                                <a href="{{url('/admin/applications')}}"><i class="fa fas fa-file" aria-hidden="true"></i>&nbsp;&nbsp;&nbsp;Applications</a>
+                            </li>
+                            <li role="presentation"
+                                {{Request::segment(2)=='serviceGroups'?$active='active':$active=''}} class="{{$active}}">
+                                <a href="{{url('/admin/serviceGroups')}}"><i class="fa fas fa-object-group" aria-hidden="true"></i>&nbsp;&nbsp;&nbsp;API Groups</a>
+                            </li>
+                            <li role="presentation"
+                                {{Request::segment(2)=='services'?$active='active':$active=''}} class="{{$active}}">
+                                <a href="{{url('/admin/services')}}"><i class="fa fas fa-cogs" aria-hidden="true"></i>&nbsp;&nbsp;&nbsp;Services</a>
+                            </li>
+                            <li role="presentation"
+                                {{Request::segment(2)=='subscriptions'?$active='active':$active=''}} class="{{$active}}">
+                                <a href="{{url('/admin/subscriptions')}}"><i class="fa fas fa-handshake-o" aria-hidden="true"></i>&nbsp;&nbsp;&nbsp;Subscriptions</a>
+                            </li>
+                            <li role="presentation"
+                                {{Request::segment(2)=='messages'?$active='active':$active=''}} class="{{$active}}">
+                                <a href="{{url('/admin/messages')}}"><i class="fa fas fa-envelope" aria-hidden="true"></i>&nbsp;&nbsp;&nbsp;Messages {!! ($unread_message_count>0)?'<span class="badge">'.$unread_message_count.'</span>':'' !!}</a>
+                            </li>
+                            <li role="presentation"
+                                {{Request::segment(2)=='logs'?$active='active':$active=''}} class="{{$active}}">
+                                <a href="{{url('/admin/logs')}}"><i class="fa fas fa-list" aria-hidden="true"></i>&nbsp;&nbsp;&nbsp;Logs</a>
+                            </li>
+                        @else
+                            <li role="presentation"
+                                {{Request::segment(2)=='home'?$active='active':$active=''}} class="{{$active}}">
+                                <a href="{{url('/developer/home')}}"><i class="fa fas fa-home" aria-hidden="true"></i>&nbsp;&nbsp;&nbsp;Dashboard</a>
+                            </li>
+                            <li role="presentation"
+                                {{Request::segment(2)=='applications'?$active='active':$active=''}} class="{{$active}}">
+                                <a href="{{url('/developer/applications')}}"><i class="fa fas fa-file" aria-hidden="true"></i>&nbsp;&nbsp;&nbsp;Applications</a>
+                            </li>
+                            <li role="presentation"
+                                {{Request::segment(2)=='serviceGroups'?$active='active':$active=''}} class="{{$active}}">
+                                <a href="{{url('/developer/serviceGroups')}}"><i class="fa fas fa-object-group" aria-hidden="true"></i>&nbsp;&nbsp;&nbsp;API Groups</a>
+                            </li>
+                            <li role="presentation"
+                                {{Request::segment(2)=='services'?$active='active':$active=''}} class="{{$active}}">
+                                <a href="{{url('/developer/services')}}"><i class="fa fas fa-cogs" aria-hidden="true"></i>&nbsp;&nbsp;&nbsp;Services</a>
+                            </li>
+                            <li role="presentation"
+                                {{Request::segment(2)=='subscriptions'?$active='active':$active=''}} class="{{$active}}">
+                                <a href="{{url('/developer/subscriptions')}}"><i class="fa fas fa-handshake-o" aria-hidden="true"></i>&nbsp;&nbsp;&nbsp;Subscriptions</a>
+                            </li>
+                            <li role="presentation"
+                                {{Request::segment(2)=='messages'?$active='active':$active=''}} class="{{$active}}">
+                                <a href="{{url('/developer/messages')}}"><i class="fa fas fa-envelope" aria-hidden="true"></i>&nbsp;&nbsp;&nbsp;Messages {!! ($unread_message_count>0)?'<span class="badge">'.$unread_message_count.'</span>':'' !!}</a>
+                            </li>
+                        @endif
+                    @endif
                 </ul>
             </div><!-- /.navbar-collapse -->
         </nav>
