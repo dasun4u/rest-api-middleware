36b678dd5cc7e814e1a0e687d816ab15e374bd87
Dasun Dissanayake <dpdasun@gmail.com>
2018-04-09 14:02:26 +0530

    Developer home application list

--- app/Http/Controllers/Developer/HomeController.php
+++ app/Http/Controllers/Developer/HomeController.php
@@ -27,7 +27,7 @@ class HomeController extends Controller
      */
     public function index()
     {
-        $pending_applications = Application::where('approved',0)->get();
+        $pending_applications = Application::where('approved',0)->where('created_by',Auth::user()->id)->get();
         $pending_services = Service::where('approved',0)->get();
         $pending_subscriptions = Subscription::where('approved',0)->where('subscribed_by',Auth::user()->id)->get();
         return view('pages.developer.home', ['applications' => $pending_applications,  'services' => $pending_services, 'subscriptions' => $pending_subscriptions]);
--- resources/views/pages/developer/home.blade.php
+++ resources/views/pages/developer/home.blade.php
@@ -13,6 +13,52 @@
                     <div class="col-sm-6">
                         <div class="col-sm-10 col-sm-offset-1">
                             <div class="panel panel-info">
+                                <div class="panel-heading">Applications to Approve</div>
+                                <div class="panel-body">
+                                    <div class="table-responsive">
+                                        <table class="table table-striped">
+                                            <thead>
+                                            <tr class="success">
+                                                <th>#</th>
+                                                <th>Name</th>
+                                                <th>Description</th>
+                                                <th>Date / Time</th>
+                                            </tr>
+                                            </thead>
+                                            <tbody>
+                                            @forelse($applications as $application)
+                                                @if($loop->index<5)
+                                                    <tr>
+                                                        <td>{{ $application->id }}</td>
+                                                        <td>{{ $application->name }}</td>
+                                                        <td>{{ str_limit($application->description, 50) }}</td>
+                                                        <td>{{ $application->created_at }}</td>
+                                                    </tr>
+                                                @else
+                                                    <tr>
+                                                        <td colspan="5">
+                                                            <a class="btn btn-primary pull-right"
+                                                               href="{{ url('admin/applications') }}">View More</a>
+                                                        </td>
+                                                    </tr>
+                                                    @break
+                                                @endif
+                                            @empty
+                                                <tr>
+                                                    <td colspan="5" class="text-center">No Applications to Approve
+                                                    </td>
+                                                </tr>
+                                            @endforelse
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-6">
+                        <div class="col-sm-10 col-sm-offset-1">
+                            <div class="panel panel-info">
                                 <div class="panel-heading">Services to Approve</div>
                                 <div class="panel-body">
                                     <div class="table-responsive">
@@ -54,6 +100,8 @@
                             </div>
                         </div>
                     </div>
+                </div>
+                <div class="row">
                     <div class="col-sm-6">
                         <div class="col-sm-10 col-sm-offset-1">
                             <div class="panel panel-info">
